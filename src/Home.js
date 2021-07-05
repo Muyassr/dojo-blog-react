50dd4fda98eb4de1ea69019bc23a9a3e36faf3fd
tamimm <tamimm@aecl.com>
2021-07-05 11:45:25 +0300

    useState Hook

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,20 @@
+import {useState} from 'react';
+
 const Home = () => {
-   const handleClick = (e) => {
-       console.log(e)
+
+   const [name, setname] = useState('Moyasr');
+   const [age, setage] = useState(33)
+   const handleClick = () => {
+        setname('razan')
+        setage(31);
    }
 
-   const handleClick2 = (name) => {
-    alert('hello '+name)
-}
-    return (
+    
+   return (
         <div className="home">
             <h2>Homepage</h2>
+            <h3>{name} is {age}</h3>
             <button onClick={handleClick}>click me</button>
-            <button onClick={()=>handleClick2('Moyasr')}>click name</button>
         </div>
     );
 }
