0967dde1a12e195d36437f60e423535da95a4892
tamimm <tamimm@aecl.com>
2021-07-05 11:25:55 +0300

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,16 @@
 const Home = () => {
-   
+   const handleClick = (e) => {
+       console.log(e)
+   }
+
+   const handleClick2 = (name) => {
+    alert('hello '+name)
+}
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>click me</button>
+            <button onClick={()=>handleClick2('Moyasr')}>click name</button>
         </div>
     );
 }
