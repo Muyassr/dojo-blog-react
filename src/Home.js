09ed8bae277541e820b650be4575a7d0314a0027
tamimm <tamimm@aecl.com>
2021-07-05 13:59:58 +0300

    output blogs list

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,21 @@
 import {useState} from 'react';
 
 const Home = () => {
-
-   const [name, setname] = useState('Moyasr');
-   const [age, setage] = useState(33)
-   const handleClick = () => {
-        setname('razan')
-        setage(31);
-   }
-
     
+    const [blogs, setBlogs] = useState([
+        {title: 'tit1', body:'sksksk1', author: 'Moyasr', id:'1'},
+        {title: 'tit2', body:'sksksk2', author: 'ali', id:'2'},
+        {title: 'tit3', body:'sksksk3', author: 'Mohammed', id:'3'}
+    ]);
+
    return (
         <div className="home">
-            <h2>Homepage</h2>
-            <h3>{name} is {age}</h3>
-            <button onClick={handleClick}>click me</button>
+            {blogs.map((blogX)=>(
+               <div className="blog-preview" key={blogX.id}>
+                   <h2>{blogX.title}</h2>
+                   <h5>writter by {blogX.author}</h5>
+               </div>
+            ))}
         </div>
     );
 }
