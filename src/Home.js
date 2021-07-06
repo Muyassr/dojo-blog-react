67e095200de0e28c38aa100cc16c7dbd1e433d2b
tamimm <tamimm@aecl.com>
2021-07-06 11:36:01 +0300

    using filter

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: 'tit1', body:'sksksk1', author: 'Moyasr', id:'1'},
         {title: 'tit2', body:'sksksk2', author: 'ali', id:'2'},
-        {title: 'tit3', body:'sksksk3', author: 'Mohammed', id:'3'}
+        {title: 'tit3', body:'sksksk3', author: 'Moyasr', id:'3'}
     ]);
 
    return (
         <div className="home">
           <BlogList blogs={blogs} title="All blogs"></BlogList>
+          <BlogList blogs={blogs.filter(blogX=>blogX.author=='Moyasr')} title="Moyasr's blogs"></BlogList>
         </div>
     );
 }
