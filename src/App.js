0d19113f64a9fdec308ea7da300615b5c81ef4f5
tamimm <tamimm@aecl.com>
2021-07-07 12:08:01 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import Home from './Home';
 import Navbar from './Navbar';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-     
-      <Navbar/>
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Switch>
+            <Route path='/'>
+              <Home></Home>
+            </Route>
+            <Route path='/ss'> <h2>sssdd</h2></Route>
+          </Switch>
+        </div>
       </div>
-     
-    </div>
+    </Router>
   );
 }
 
